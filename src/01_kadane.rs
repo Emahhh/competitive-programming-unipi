494062aff2efd92fed4675a429f0e4ff378a4fe1
Emanuele B <buonaccorsiemanuele@gmail.com>
2023-09-25 20:44:24 +0200

    fixed project organization

--- src/01_kadane.rs
+++ src/01_kadane.rs
@@ -1,21 +1,28 @@
 // https://leetcode.com/problems/maximum-subarray/
+struct Solution{
+
+}
 
 impl Solution {
 
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
         use std::cmp::max;
 
-        let mut maxSubarrSum : i32;
-        let mut lastMaxSum : i32;
+        let mut max_subarr_sum : i32;
+        let mut last_max_sum : i32;
 
-        lastMaxSum = nums[0];
-        maxSubarrSum = nums[0];
+        last_max_sum = nums[0];
+        max_subarr_sum = nums[0];
 
         for i in 1..nums.len() {
-            lastMaxSum = max(nums[i], lastMaxSum + nums[i]);
-            maxSubarrSum = max(maxSubarrSum, lastMaxSum);
+            last_max_sum = max(nums[i], last_max_sum + nums[i]);
+            max_subarr_sum = max(max_subarr_sum, last_max_sum);
         }
         
-        return maxSubarrSum;
+        return max_subarr_sum;
     }
+}
+
+fn main() {
+    println!("{}", Solution::max_sub_array(vec![-2,1,-3,4,-1,2,1,-5,4]));
 }
