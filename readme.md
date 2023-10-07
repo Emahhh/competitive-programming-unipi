2038f4c460a24fe24e967a0c948f32bca3160d39
Emanuele B <buonaccorsiemanuele@gmail.com>
2023-10-07 18:40:54 +0200

    trees

--- readme.md
+++ readme.md
@@ -5,12 +5,35 @@ Repository containing my solutions to various exercises for the course "Competit
 - Naming convention: `<lesson-number>_name.rs`
 - After creating a new source file, rember to add it to the `Cargo.toml` file. This will enable VSCode's linting using `clippy`.
 
-## Testing
-- write a test function and put `#[test]` before its declaration
-- use some macros like `assert!` and `assert_eq!` 
-- run `cargo test`
-
-## Others
+## Useful commands
 
 - use `cargo fmt` to format your code.
 - use `cargo clippy` to check your code.
+
+## Testing
+
+Write some tests in the same file using this format:
+
+```rust
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_multiplication() {
+        assert_eq!(2 * 3, 6);
+    }
+
+    #[test]
+    fn test_division() {
+        assert_eq!(10 / 2, 5);
+    }
+}
+```
+
+Some commonly used **assertion** macros include:
+- `assert_eq!(left, right)` - Asserts that `left` is equal to `right`.
+- `assert_ne!(left, right)` - Asserts that `left` is not equal to `right`.
+- `assert!(condition)` - Asserts that `condition` is true.
+- `assert!(condition, message)` - Asserts that `condition` is true and displays the `message` if the assertion fails.
+
+Finally, run `cargo test` or use VSCode's "tests" button.
+
---
+++ src/04_trees.rs
@@ -0,0 +1,65 @@
+struct TreeNode {
+    val: i32,
+    left: Option<Box<TreeNode>>,
+    right: Option<Box<TreeNode>>,
+}
+
+fn tree_size(n: Option<Box<TreeNode>>) -> i32 {
+    if n.is_none() {
+        return 0;
+    }
+
+    let node = n.unwrap();
+    let size_left = tree_size(node.left);
+    let size_right = tree_size(node.right);
+
+    1 + size_left + size_right
+}
+
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+    #[test]
+    fn test_tree_size_empty() {
+        let n: Option<Box<TreeNode>> = None;
+        let result = tree_size(n);
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn test_tree_size_single_node() {
+        let n = Some(Box::new(TreeNode {
+            val: 10,
+            left: None,
+            right: None,
+        }));
+        let result = tree_size(n);
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn test_tree_size_complex_tree() {
+        let n = Some(Box::new(TreeNode {
+            val: 1,
+            left: Some(Box::new(TreeNode {
+                val: 2,
+                left: None,
+                right: None,
+            })),
+            right: Some(Box::new(TreeNode {
+                val: 3,
+                left: Some(Box::new(TreeNode {
+                    val: 4,
+                    left: None,
+                    right: None,
+                })),
+                right: None,
+            })),
+        }));
+        let result = tree_size(n);
+        assert_eq!(result, 4);
+    }
+
+}
