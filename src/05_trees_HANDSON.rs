7fc423c329b636cb1a0b4f876f6123c7a9848767
Emanuele B <buonaccorsiemanuele@gmail.com>
2023-10-09 20:42:11 +0200

    spostato in helper function

--- src/05_trees_HANDSON.rs
+++ src/05_trees_HANDSON.rs
@@ -1,6 +1,5 @@
-
 /// Basic Binary Tree implementation
-/// 
+///
 /// # Author
 /// - [Professor Venturini](https://pages.di.unipi.it/rossano/blog/2023/handson12324/)
 #[allow(dead_code)]
@@ -10,7 +9,7 @@ mod trees {
         pub id_left: Option<usize>,
         pub id_right: Option<usize>,
     }
-    
+
     impl Node {
         fn new(key: u32) -> Self {
             Self {
@@ -20,11 +19,11 @@ mod trees {
             }
         }
     }
-    
+
     pub struct Tree {
         nodes: Vec<Node>,
     }
-    
+
     /// This a representation of a tree.
     /// Every node has an implicity id, which is its position on the vector `nodes`.
     /// Every node has a key and at most two children. The ids of the children are
@@ -39,7 +38,7 @@ mod trees {
         pub fn get_node(&self, id: usize) -> Option<&Node> {
             self.nodes.get(id)
         }
-    
+
         /// Adds a child to the node with `parent_id` and returns the id of the new node.
         /// The new node has the specified `key`. The new node is the left child of the node `parent_id`
         /// iff `is_left` is `true`, the right child otherwise.
@@ -62,104 +61,89 @@ mod trees {
                     "Parent node has the right child already set"
                 );
             }
-    
+
             let child_id = self.nodes.len();
             self.nodes.push(Node::new(key));
-    
+
             let child = if is_left {
                 &mut self.nodes[parent_id].id_left
             } else {
                 &mut self.nodes[parent_id].id_right
             };
-    
+
             *child = Some(child_id);
-    
+
             child_id
         }
-    
+
         /// Returns the sum of all the keys in the tree
         pub fn sum(&self) -> u32 {
             self.rec_sum(Some(0))
         }
-    
+
         /// A private recursive function that computes the sum of
         /// nodes in the subtree rooted at `node_id`.
         fn rec_sum(&self, node_id: Option<usize>) -> u32 {
             if let Some(id) = node_id {
                 assert!(id < self.nodes.len(), "Node id is out of range");
                 let node = &self.nodes[id];
-    
+
                 let sum_left = self.rec_sum(node.id_left);
                 let sum_right = self.rec_sum(node.id_right);
-    
+
                 return sum_left + sum_right + node.key;
             }
-    
+
             0
         }
     }
-    
-
 }
 
+impl trees::Tree {
+    /// Exercise 1
+    /// Checks if the binary tree is a Binary Search Tree.
+    pub fn is_bst(&self) -> bool {
+        self.helper_rec_is_bst(Some(0)).0
+    }
 
-/// Exercise 1
-/// Checks if the binary tree is a Binary Search Tree.
-/*
-that is: for every node, all the vals in the left subtree are smaller than me, and all the vals in the right are greater
-=
-the max val in the left subtree is ≤ than me, and the min val in the right subtree is ≥ than me
+    fn helper_rec_is_bst(&self, curr_root_id: Option<usize>) -> (bool, u32, u32) {
+        const NEG_INF: u32 = u32::MIN; // since we are dealing with u32, the value is actually 0
+        const INF: u32 = u32::MAX;
 
-```jsx
-function isBST(t): [bool, max, min]{
-	if t == null return [true, -Inf, +Inf];
-
-	let [isLeftBST, maxLeft, _] = isBST(t.left);
-	let [isRightBST, _, minRight] = isBST(t.right);
-	
-	if (!isLeftBST) return false;
-	if (!isRightBST) return false;
-
-	let amIBST : bool = (maxLeft <= t.val  && t.val => minRight);
-	let myMax = max(t.val, maxLeft, minRight);
-	let myMin = min(t.val, maxLeft, minRight);
-	return [amIBST, myMax, myMin]; // if not null
-
-}
-*/
-fn is_bst(t: &trees::Tree) -> (bool, u32, u32) {
-    const NEG_INF: u32 = u32::MIN; // since we are dealing with u32, the value is actually 0
-    const INF: u32 = u32::MAX;
-
-    let first_node: Option<&trees::Node> = t.get_node(0);
-
-    match first_node {
-        None => (true, NEG_INF, INF),
-
-        Some(node) => {
-            let (is_left_bst, max_left, min_right) = is_bst(t);
-            let (is_right_bst, max_right, min_left) = is_bst(t);
-
-            if !is_left_bst || !is_right_bst {
-                return (false, NEG_INF, INF);
-            }
-            
-            let am_i_bst : bool = (max_left <= node.key && node.key <= min_right);
-            let my_max = core::cmp::max(core::cmp::max(node.key, max_left), min_right);
-            let my_min = core::cmp::min(core::cmp::min(node.key, max_left), min_right);
-            return (am_i_bst, my_max, my_min);
+        if curr_root_id.is_none() {
+            return (true, NEG_INF, INF);
         }
+
+        let current_root_opt: Option<&trees::Node> = self.get_node(curr_root_id.unwrap());
+        if current_root_opt.is_none() {
+            return (false, NEG_INF, INF);
+        }
+
+        let root: &trees::Node = current_root_opt.unwrap();
+
+        let (is_left_bst, max_left, _ ) = self.helper_rec_is_bst(root.id_left);
+        let (is_right_bst, _ , min_right) = self.helper_rec_is_bst(root.id_right);
+
+        if !is_left_bst || !is_right_bst {
+            return (false, NEG_INF, INF);
+        }
+
+        let am_i_bst: bool = max_left <= root.key && root.key <= min_right;
+
+        let my_max = core::cmp::max(core::cmp::max(root.key, max_left), min_right);
+        let my_min = core::cmp::min(core::cmp::min(root.key, max_left), min_right);
+        return (am_i_bst, my_max, my_min);
+        
         
     }
-    
-}
 
+
+
+}
 
 pub fn main() {
     println!("Hello, trees handson!");
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -168,8 +152,11 @@ mod tests {
     #[test]
     fn test_is_bst_1() {
         let mut tree = trees::Tree::with_root(10);
-        assert!(is_bst(&tree).0);
+        assert!(tree.is_bst());
+
+        tree.add_node(0, 5, true); // id 1
+        tree.add_node(0, 22, false); // id 2
+
+        assert!(tree.is_bst());
     }
-
 }
-
