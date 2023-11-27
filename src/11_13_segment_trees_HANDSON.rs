48e71c38de759fa1b67a340a087c00854e148b96
Emanuele B. <buonaccorsiemanuele@gmail.com>
2023-11-27 17:36:07 +0100

    importing fenwick

--- src/11_13_segment_trees_HANDSON.rs
+++ src/11_13_segment_trees_HANDSON.rs
@@ -372,11 +372,78 @@ mod tests {
 
 
 
+// TODO: author
+pub mod fenwick {
+    #[derive(Debug)]
+    pub struct FenwickTree {
+        tree: Vec<i64>,
+    }
+    
+    impl FenwickTree {
+        pub fn with_len(n: usize) -> Self {
+            Self {
+                tree: vec![0; n + 1],
+            }
+        }
+    
+        pub fn len(&self) -> usize {
+            self.tree.len() - 1
+        }
+    
+        /// Indexing is 0-based, even if internally we use 1-based indexing
+        pub fn add(&mut self, i: usize, delta: i64) {
+            let mut i = i + 1; 
+            assert!(i < self.tree.len());
+    
+            while i < self.tree.len() {
+                self.tree[i] += delta;
+                i = Self::next_sibling(i);
+            }
+        }
+    
+        /// Indexing is 0-based, even if internally we use 1-based indexing
+        pub fn sum(&self, i: usize) -> i64 {
+            let mut i = i + 1;  
+    
+            assert!(i < self.tree.len());
+            let mut sum = 0;
+            while i != 0 {
+                sum += self.tree[i];
+                i = Self::parent(i);
+            }
+    
+            sum
+        }
+    
+        pub fn range_sum(&self, l: usize, r: usize) -> i64 {
+            self.sum(r) - if l == 0 { 0 } else { self.sum(l - 1) }
+        }
+    
+        fn isolate_trailing_one(i: usize) -> usize {
+            if i == 0 {
+                0
+            } else {
+                1 << i.trailing_zeros()
+            }
+        }
+    
+        fn parent(i: usize) -> usize {
+            i - Self::isolate_trailing_one(i)
+        }
+    
+        fn next_sibling(i: usize) -> usize {
+            i + Self::isolate_trailing_one(i)
+        }
+    }
+}
+
 
 
 
 /// EXERCISE 2 (is_there)
 pub mod ex_2_is_there {
+
     
+
 
 }
